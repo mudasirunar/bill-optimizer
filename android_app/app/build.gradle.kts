035c3e63d5936b36130d8fdeb4876e34bee9d366
Mudasir Ali <unarmudasir@gmail.com>
2026-07-19 01:39:17 +0500

    chore: bump version code to 3 and version name to 2.0.1

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.fyp.aibilloptimizer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
