f6f1a140aa36cf98ea74a804eca5c3b377c3ce58
Mudasir Ali <unarmudasir@gmail.com>
2026-07-19 02:29:23 +0500

    chore: update version code and version name to 4 and 2.0.2 respectively

--- android_app/app/build.gradle.kts
+++ android_app/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.fyp.aibilloptimizer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "2.0.1"
+        versionCode = 4
+        versionName = "2.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
